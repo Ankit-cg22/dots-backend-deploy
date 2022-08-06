eb66c20f359ae38567269b009cefb9561828703f
Ankit-cg22 <ankitdas.cg22@gmail.com>
2022-08-06 18:12:49 +0530

    changes

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
+app.get('/' , (req, res)=>{
+  console.log("Welcome to dots api !")
+})
+
 app.use('/posts', postRoutes); 
 app.use('/user' , userRoutes);
 
